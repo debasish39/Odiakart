9bb33575987cb1b3ed4a0ea2c0254ce7fc6430fd
Debasish Panda <156610494+debasish39@users.noreply.github.com>
2026-09-20 00:04:33 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -971,7 +971,7 @@ const showSearchNavbar =
 
               <Route
                 path="/sign-in/*"
-                element={<SignInPage />}
+                element={<PhoneLogin />}
               />
 
               <Route
